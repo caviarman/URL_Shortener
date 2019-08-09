38fff3757eaa254d3f20b4814f69a86e81b4053f
Vladimir Ilin <mark09072010@gmail.com>
2019-08-09 08:17:19 +0300

    try heroku deploy

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const express =  require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static('./dist/urlShort'));
+app.use(express.static('../dist/urlShort'));
 
 app.get('/api/hello', function(req, res) {
     res.send('<h1>Hello World</h1>');
 });
 app.get('/*', function(req, res) {
-    res.sendFile(path.join('./dist/urlShort/index.html'));
+    res.sendFile(path.join('../dist/urlShort/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
